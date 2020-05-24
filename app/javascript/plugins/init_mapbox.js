c09d35f03e78a85ed86752561689d64235a357a5
yusuke ishida <ishidayusuke01@gmail.com>
2020-05-24 23:13:50 +0900

    Update node js

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,5 +1,4 @@
 import mapboxgl from 'mapbox-gl';
-import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
 
 const initMapbox = () => {
@@ -21,19 +20,14 @@ const initMapbox = () => {
 
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
       new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
-        .setPopup(popup)
         .addTo(map);
     });
 
 
     fitMapToMarkers(map, markers);
-    map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
-                                          mapboxgl: mapboxgl }));
   }
-
 };
 
 export { initMapbox };

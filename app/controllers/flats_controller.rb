080c522003adaafe14248c09fecb477a5ee2dd45
yusuke ishida <ishidayusuke01@gmail.com>
2020-05-24 23:06:58 +0900

    Added some style to the map

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -8,7 +8,9 @@ class FlatsController < ApplicationController
     @markers = @flats.map do|flat|
       {
         lat: flat.latitude,
-        lng: flat.longitude
+        lng: flat.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { flat: flat })
+
       }
     end
   end
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,7 +1,9 @@
 import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
 
 const initMapbox = () => {
+  // Selet map
   const mapElement = document.getElementById('map');
   console.log('working')
   const fitMapToMarkers = (map, markers) => {
@@ -19,14 +21,19 @@ const initMapbox = () => {
 
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
       new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(popup)
         .addTo(map);
     });
 
 
     fitMapToMarkers(map, markers);
+    map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+                                          mapboxgl: mapboxgl }));
   }
+
 };
 
 export { initMapbox };
